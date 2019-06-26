7dd66fb506a335fdec62ceee98e60b4b5b7f2c1c
Epicalert <epicalert68@gmail.com>
2019-06-26 15:59:47 +0800

    added pause after exiting runtts

--- runtts.bat
+++ runtts.bat
@@ -2,3 +2,4 @@
 set /p intext="input text: "
 py -3 ttsfbfe/tts.py "%intext%"
 echo Output is in output.wav
+pause
--- runtts.sh
+++ runtts.sh
@@ -2,3 +2,4 @@ echo input text:
 read intext
 python3 ttsfbfe/tts.py "$intext"
 echo Output is in output.wav
+read -p "Press enter to continue"
